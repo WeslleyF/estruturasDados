aa4b2f450e5d125d7b0b8ba1d49be31a8c431a21
WeslleyF <38595157+WeslleyF@users.noreply.github.com>
2024-05-04 18:18:03 +0000

    Versão inicial

---
+++ Modelo/Atividade01.cs
@@ -0,0 +1,10 @@
+namespace Modelo {
+  public static class Atividade01{
+    //Escreva um algoritmo para calcular a soma de dois 
+    //números inteiros.
+
+    public static double Somar(double n1, double n2){
+      return n1 + n2;
+    }
+  }    
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Modelo;
+
+double N1 = Convert.ToDouble(Console.ReadLine());
+double N2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Atividade01.Somar(N1, N2));
